694cb4522fc0a97d5f4f19fc438b085529acb5d1
JonahLib <libretto.jonah@gmail.com>
2025-01-17 12:57:34 +0000

    fix yup add more fields

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import ChipsInput from "@/components/chips_input";
 import DropdownInput from "@/components/dropdown_input";
 import Form from "@/components/form";
 import FormProvider from "@/components/form_provider";
+import TextInput from "@/components/text_input";
 import { FORM_SCHEMA, TEST_CHIPS } from "@/constants/form";
 
 export default function Home() {
@@ -12,29 +13,42 @@ export default function Home() {
     <FormProvider
       defaultValues={{
         tac: false,
-        cats: [],
-        brandName: "",
-        country: "",
-        brandDescription: "",
+        chips: [],
+        text: "",
+        dropdown: "",
         ccgs: "",
+        date: "",
       }}
       yupSchema={FORM_SCHEMA}
     >
       <Form title="Form" onSubmit={(data) => console.log(data)}>
+        <TextInput
+          name="text"
+          type="text"
+          label="Text Input"
+          placeholder="Place holder"
+        />
         <DropdownInput
-          name="country"
-          label="Country"
+          name="dropdown"
+          label="Dropdown Input"
           options={[
-            { name: "United Kingdom", value: "UK" },
-            { name: "United States", value: "US" },
+            { name: "Option 1", value: "1" },
+            { name: "Option 2", value: "2" },
+            { name: "Option 3", value: "3" },
           ]}
         />
         <ChipsInput
-          title="Sub-categories"
+          title="Chips Input"
           description="please select up to 3"
-          name="cats"
+          name="chips"
           options={TEST_CHIPS}
           maxSelect={3}
+        />
+        <TextInput
+          name="date"
+          type="date"
+          label="Date Input"
+          placeholder="Your Brand Name"
         />
         <CheckboxInput name="tac" label="I agree to the terms and conditions" />
       </Form>
--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -9,14 +9,14 @@ const Form = ({ title, onSubmit, children }: FormProps) => {
   return (
     <form
       onSubmit={handleSubmit((data) => onSubmit(data))}
-      className="w-full flex flex-col justify-center gap-6 mx-auto my-4 p-2"
+      className="w-full flex flex-col justify-center gap-6 mx-auto my-4 p-2 max-w-[700px]"
     >
       <Typography className="self-center" variant="header" fontWeight="bold">
         {title}
       </Typography>
       {children}
 
-      <Button className="" type="submit">
+      <Button className="text-black" type="submit">
         Submit
       </Button>
     </form>
